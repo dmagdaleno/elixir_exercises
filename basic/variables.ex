b4a282dcbdeb2b19dfd8261998b225fb39a87560
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-27 22:56:07 -0300

    use more variables

--- basic/variables.ex
+++ basic/variables.ex
@@ -1,3 +1,4 @@
 distance = 200
 time = 4
-"The distance is #{distance} km, the time is #{time} hours and the average velocity is #{distance/time} km/h"
+velocity = distance/time
+"The distance is #{distance} km, the time is #{time} hours and the average velocity is #{velocity} km/h"
