c9711700e8fedaa1297b348b9720c49b744860b1
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-28 23:25:52 -0300

    fix script and comment results

--- pattern_matching/matching_lists.exs
+++ pattern_matching/matching_lists.exs
@@ -1,8 +1,11 @@
-iex(1)> [ first, second | rest ] = [:a, :b, :c, :d]
-[:a, :b, :c, :d]
-iex(2)> first
-:a
-iex(3)> second 
-:b
-iex(4)> rest
-[:c, :d]
+[ first, second | rest ] = [:a, :b, :c, :d]
+# [:a, :b, :c, :d]
+
+first
+# :a
+
+second 
+# :b
+
+rest
+# [:c, :d]
