86f1fba57bd5bbc6eaf3525af98e3f8c7b4ca18d
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-29 20:52:50 -0300

    use guard clause to verify if param is positive

--- pattern_matching/ability_modifier_v2.exs
+++ pattern_matching/ability_modifier_v2.exs
@@ -4,7 +4,7 @@ result = case Integer.parse(user_input) do
 
   :error -> "Invalid ability score: #{user_input}"
   
-  {ability_score, _} ->
+  {ability_score, _} when ability_score >= 0 ->
     ability_modifier = (ability_score - 10) / 2
     "Your ability modifier is #{ability_modifier}"
 
