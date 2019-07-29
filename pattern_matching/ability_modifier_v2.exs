2dc8687f9238444869c60507d56d12aef2bf72b1
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-29 20:50:10 -0300

    refactor using case return

--- pattern_matching/ability_modifier_v2.exs
+++ pattern_matching/ability_modifier_v2.exs
@@ -1,11 +1,13 @@
 user_input = IO.gets "Write your ability score:\n"
 
-case Integer.parse(user_input) do
+result = case Integer.parse(user_input) do
 
-  :error -> IO.puts "Invalid ability score: #{user_input}"
+  :error -> "Invalid ability score: #{user_input}"
   
   {ability_score, _} ->
     ability_modifier = (ability_score - 10) / 2
-    IO.puts "Your ability modifier is #{ability_modifier}"
+    "Your ability modifier is #{ability_modifier}"
 
 end
+
+IO.puts result
