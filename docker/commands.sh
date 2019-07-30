909d2d99b6b2f02c36d26379708c00874aca3379
dmagdaleno <diegommagdaleno@gmail.com>
2019-07-30 13:49:41 -0300

    docker commands

--- docker/commands.sh
+++ docker/commands.sh
@@ -3,3 +3,5 @@ docker volume create workvol
 docker run -it --rm --name elixir \
  -v workvol:/home/work/elixir_exercises \
  elixir:1.9.0-alpine
+
+docker run -it --rm --name elixir-inst1 -v "$PWD":/usr/src/myapp -w /usr/src/myapp elixir:1.9.0-alpine elixir recursion/sum_up.exs
