df2483a2d5d5255fcc6d1b73c83ef517bc9968b0
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-30 22:05:06 -0300

    remove helper number to initiate recursion

--- recursion/my_list.ex
+++ recursion/my_list.ex
@@ -1,9 +1,8 @@
 defmodule MyList do
   def max([]), do: nil
   def max([x]), do: x
-  def max(list) do
-    find_max(0, list)
-  end
+  def max([first, second | rest]) when first >= second, do: find_max(first, rest)
+  def max([first, second | rest]) when first < second, do: find_max(second, rest)
 
   defp find_max(max, []), do: max
   defp find_max(max, [head | tail]) when max >= head, do: find_max(max, tail)
@@ -11,9 +10,8 @@ defmodule MyList do
 
   def min([]), do: nil
   def min([x]), do: x
-  def min(list) do
-    find_min(0, list)
-  end
+  def min([first, second | rest]) when first <= second, do: find_min(first, rest)
+  def min([first, second | rest]) when first > second, do: find_min(second, rest)
 
   defp find_min(min, []), do: min
   defp find_min(min, [head | tail]) when min <= head, do: find_min(min, tail)
