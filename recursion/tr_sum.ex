d6e3c1b2186364c05e12cdb5e2caa085a8818e59
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-30 22:37:06 -0300

    avoid unnecessary iteration

--- recursion/tr_sum.ex
+++ recursion/tr_sum.ex
@@ -1,6 +1,7 @@
 defmodule TRSum do
   def up_to(0), do: 0
-  def up_to(n), do: up_to(n, 0)
-  def up_to(0, acc), do: acc
-  def up_to(n, acc), do: up_to(n - 1, n + acc)
+  def up_to(n), do: up_to_acc(n - 1, n)
+
+  defp up_to_acc(0, acc), do: acc
+  defp up_to_acc(n, acc), do: up_to_acc(n - 1, n + acc)
 end
