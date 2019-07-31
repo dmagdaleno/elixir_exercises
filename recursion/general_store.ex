afc0f576f0b50155b57ae70151353b40e6421712
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-30 22:16:33 -0300

    filter itens by magic flag

---
+++ recursion/general_store.ex
@@ -0,0 +1,23 @@
+defmodule GeneralStore do
+
+  def filter_items([], _), do: []
+  
+  def filter_items([item = %{magic: item_magic} | rest], magic: filter_magic)
+      when item_magic == filter_magic do
+    [item | filter_items(rest, magic: filter_magic)]
+  end
+
+  def filter_items([_ | rest], magic: filter_magic) do
+    filter_items(rest, magic: filter_magic)
+  end
+
+  def test_data do
+    [
+      %{title: "Longsword", price: 50, magic: false},
+      %{title: "Healing Potion", price: 60, magic: true},
+      %{title: "Rope", price: 10, magic: false},
+      %{title: "Dragon's Spear", price: 100, magic: true},
+    ]
+  end
+
+end
