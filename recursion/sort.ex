3261941b6abdd73faaca6ee670885a92a647ccff
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-30 22:21:58 -0300

    sort in descending order

--- recursion/sort.ex
+++ recursion/sort.ex
@@ -20,4 +20,24 @@ defmodule Sort do
     [head_b | merge(list_a, tail_b)]
   end
 
+  def descending([]), do: []
+  def descending([a]), do: [a]
+  def descending(list) do
+    half_size = div(Enum.count(list), 2)
+    {list_a, list_b} = Enum.split(list, half_size)
+    merge_d(
+      descending(list_a),
+      descending(list_b)
+    )
+  end
+
+  defp merge_d([], list_b), do: list_b
+  defp merge_d(list_a, []), do: list_a
+  defp merge_d([head_a | tail_a], list_b = [head_b | _]) when head_a >= head_b do
+    [head_a | merge_d(tail_a, list_b)]
+  end
+  defp merge_d(list_a = [head_a | _], [head_b | tail_b]) when head_a < head_b do
+    [head_b | merge_d(list_a, tail_b)]
+  end
+
 end
