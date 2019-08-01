b9baf6ae74b097dd955b0483ee2b122d715d959e
dmagdaleno <diegommagdaleno@gmail.com>
2019-08-01 16:47:17 -0300

    add other docker execution command

--- docker/v1/commands.sh
+++ docker/v1/commands.sh
@@ -3,3 +3,7 @@ docker build docker/v1/ -t dmagdaleno/elixir:v1
 docker run -it --rm \
  --volume=/home/dmagdaleno/work/elixir/volume:/home/work/ \
  dmagdaleno/elixir:v1 bash
+
+docker run -it --rm \
+ --volume=/home/dmagdaleno/study/elixir/:/home/work/ \
+ dmagdaleno/elixir:v1 bash
