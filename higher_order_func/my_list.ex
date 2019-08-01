87c4d119f9ff985101c7e522e806aed35db724af
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-31 21:52:51 -0300

    implement map function

--- higher_order_func/my_list.ex
+++ higher_order_func/my_list.ex
@@ -4,7 +4,15 @@ defmodule MyList do
     function.(head)
     each(tail, function)
   end
+
+  def map([], _function), do: []
+  def map([head | tail], function) do
+    [function.(head) | map(tail, function)]
+  end
 end
 
-# MyList.each(enchanted_items, fn item -> IO.puts item.title end)
-# MyList.each(enchanted_items, fn item -> IO.puts String.upcase(item.title) end)
+# MyList.each(items, fn item -> IO.puts item.title end)
+# MyList.each(items, fn item -> IO.puts String.upcase(item.title) end)
+#
+# MyList.map(items, fn item -> %{title: item.title, price: item.price * 1.1} end)
+# MyList.map(items, fn item -> update_in(item.price, &(&1 * 1.1)) end)
